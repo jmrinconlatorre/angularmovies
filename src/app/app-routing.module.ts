2771618be8bf62c19afb3b28e2e89d5e09551213
juanma <jm.rincon.latorre@gmail.com>
2019-05-21 12:50:28 +0200

    3 links created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MoviesCategoryComponent } from './movies-category/movies-category.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [//definimos las rutas, propiedad path y propiedad component
+  {path:'movies/:category',component: MoviesCategoryComponent},
+  {path:'movie/:id',component: MovieDetailComponent},
+  {path: '**', redirectTo: '/movies/popular', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MoviesCategoryComponent } from './movies-category/movies-category.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesCategoryComponent,
+    MovieDetailComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
